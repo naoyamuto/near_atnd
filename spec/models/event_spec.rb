d2f7e195cfc3a8918758c768b92d283a4a25fd4a
naoya_muto <naoya.mutou@gmail.com>
2016-05-17 15:08:56 +0900

    wip

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'Association' do
+
+  end
+
+  describe 'Validation' do
+
+    context 'title' do
+      it { should validate_presence_of :title }
+    end
+
+    context 'hold_at' do
+      it 'should be present' do
+        should validate_presence_of :hold_at
+      end
+    end
+
+    context 'capacity' do
+      it 'should be present' do
+        should validate_presence_of :capacity
+      end
+    end
+
+    context 'location' do
+      it 'should be present' do
+        should validate_presence_of :location
+      end
+    end
+
+    context 'owner' do
+      it 'should be present' do
+        should validate_presence_of :owner
+      end
+    end
+
+    context 'description' do
+      it 'should be present' do
+        should validate_presence_of :description
+      end
+    end
+
+  end
+
+
+
+
+
+
+
 end
