4d2d3df116cee1076e14ed8ffbec220fcf8fd5cf
naoya_muto <naoya.mutou@gmail.com>
2016-05-19 12:26:24 +0900

    wip

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
-  before_action :logged_in_user, only:[:new, :edit, :update, :create, :destroy]
+  # before_action :logged_in_user, only:[:new, :edit, :update, :create, :destroy]
+  before_action :logged_in_user, except:[:index, :show]
 
   def index
     @events = Event.all
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  # before_action :logged_in_user
 
   def show
     @user = User.find(params[:id])
